3ee93d7d2e75ccde2c8760aed7f0ea04a33e6370
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2019-09-04 20:19:07 -0500

    [+] Settings: Fija Puerto de aplicación

--- src/app.js
+++ src/app.js
@@ -2,4 +2,7 @@
 const express = require( 'express' ),
       app = express();
 
+// Settings
+app .set( 'port', process .env .PORT || 4000 );     // app .set() define valores por defecto para la aplicación con el nombre 'port'
+                                                    // Considera Puerto para servicios en la nube (Si existe que lo use, si no que use por defecto el 8082)
 module .exports = app;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 /** Principal File */
 require( 'dotenv' ) .config();  // Importa el paquete para obtener las variables de entorno para SO
-const app = require( './app' ), // Importa Express a la Aplicación
-      port = process .env .MONGODB_PORT               // 'process': Objeto global de Node
-        ? process .env .MONGODB_PORT                  //    En el root del proyecto se debe crear el archivo .env con las variables de entorno deseadas (No debe agregarse al repositorio por seguridad)
-        : 8082;  
+const app = require( './app' ); // Importa Express a la Aplicación  
 require( './database' );        // Importa la base de datos
 
 // Inicia la aplicación usando una función Asincrona
 async function main() {
     // Ejecuta el Servidor de Node
-    await app .listen( port );  
-    console .log( `Server on port ${ port }` );
+    await app .listen( app .get( 'port' ) );                    // Obtiene los valores definidos para la aplicación con el nombre 'port'
+    console .log( `Server on port ${ app .get( 'port' ) }` );
 }
 
 main();
