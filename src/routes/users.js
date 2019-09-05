b0a7a7fb67dbc26c952d3a903052b08c77346518
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2019-09-05 18:33:30 -0500

    [t] Routes (users): get, post, put, delete (con rutas específicas)

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,14 @@
 const { Router } = require( 'express' ),
       router = Router();
-
+      
 // Escucha la ruta
 router .route( '/' ) 
-    .get( ( request, response ) => response .json({ message: 'GET - API Users' }) ) 
-    .post( ( request, response ) => response .json({ message: 'POST - API Users' }) )
-    .put( ( request, response ) => response .json({ message: 'PUT - API Users' }) )
-    .delete( ( request, response ) => response .json({ message: 'DELETE - API Users' }) ); 
+    .get( ( request, response ) => response .json({ message: 'GET - API Users' }) )         // Recupera todos los recursos (Registros)
+    .post( ( request, response ) => response .json({ message: 'POST - API Users' }) )       // Crea o envia una entidad a un recurso en específico 
+    
+router .route( '/:id' )
+    .get( ( request, response ) => response .json({ message: 'GET - API Users' }) )         // Recupera un recurso específico
+    .put( ( request, response ) => response .json({ message: 'PUT - API Users' }) )         // Reemplaza el recurso específico de destino
+    .delete( ( request, response ) => response .json({ message: 'DELETE - API Users' }) );  // Borra un recurso en específico
 
 module .exports = router;
