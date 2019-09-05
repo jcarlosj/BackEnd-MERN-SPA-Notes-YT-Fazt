7c41848afe1f2a2fbfb54651b8528e02750e8eb2
Juan Carlos Jimenez Gutierrez <jcjimenez29@misena.edu.co>
2019-09-05 18:12:00 -0500

    [t] Routes (users): get, post, put, delete (pasando una cadena)
    Testing:
       Creamos los diferentes métodos de petición con el Router de
       Express (get, post, put, delete) y probamos con un entorno de
       desarrollo de API (Postman, Insomnia, etc) que funcionan
       correctamente, enviando un mensaje.

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,10 @@ const { Router } = require( 'express' ),
       router = Router();
 
 // Escucha la ruta
-router .route( '/' ) .get( ( request, response ) => response .send( 'API Users' ) ); 
+router .route( '/' ) 
+    .get( ( request, response ) => response .send( 'GET - API Users' ) ) 
+    .post( ( request, response ) => response .send( 'POST - API Users' ) )
+    .put( ( request, response ) => response .send( 'PUT - API Users' ) )
+    .delete( ( request, response ) => response .send( 'DELETE - API Users' ) ); 
 
 module .exports = router;
